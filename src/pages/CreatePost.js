c07d28d4ad240dd760e3e33aa55056e278b566c3
Abhradip Saha <110524706+abhradip-saha@users.noreply.github.com>
2024-01-30 14:03:15 +0530

    Update CreatePost.js

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -17,7 +17,7 @@ export default function CreatePost() {
     data.set('content', content);
     data.set('file', files[0]);
     ev.preventDefault();
-    const response = await fetch('https://blog-backend-auth-alpha.vercel.app/post',  {
+    const response = await fetch('https://blog-pki8.onrender.com/post',  {
       method: 'POST',
       body: data,
       credentials: 'include',
