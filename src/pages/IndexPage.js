6e3c8c30ac9c03961e7d850f6caea14cb4341dd7
Abhradip Saha <110524706+abhradip-saha@users.noreply.github.com>
2024-01-30 14:08:17 +0530

    Update IndexPage.js

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -4,7 +4,7 @@ import {useEffect, useState} from "react";
 export default function IndexPage() {
   const [posts,setPosts] = useState([]);
   useEffect(() => {
-    fetch('https://blog-backend-auth-alpha.vercel.app/post').then(response => {
+    fetch('https://blog-pki8.onrender.com/post').then(response => {
       response.json().then(posts => {
         setPosts(posts);
       });
