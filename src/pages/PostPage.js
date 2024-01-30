68f329a7e2838e59633a1375d8ceb9f8bc2ae5b0
Abhradip Saha <110524706+abhradip-saha@users.noreply.github.com>
2024-01-30 13:41:13 +0530

    Update PostPage.js

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -9,7 +9,7 @@ export default function PostPage() {
   const {userInfo} = useContext(UserContext);
   const {id} = useParams();
   useEffect(() => {
-    fetch(`https://blog-64s5.onrender.com/post/${id}`)
+    fetch(`https://blog-backend-auth-alpha.vercel.app/post/${id}`)
       .then(response => {
         response.json().then(postInfo => {
           setPostInfo(postInfo);
@@ -35,7 +35,7 @@ export default function PostPage() {
         </div>
       )}
       <div className="image">
-        <img src={`https://blog-64s5.onrender.com/${postInfo.cover}`} alt=""/>
+        <img src={`https://blog-backend-auth-alpha.vercel.app/${postInfo.cover}`} alt=""/>
       </div>
       <div className="content" dangerouslySetInnerHTML={{__html:postInfo.content}} />
     </div>
