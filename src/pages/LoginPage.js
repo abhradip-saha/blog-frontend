73f09bd59daa60d9778df6f14dc644c103842394
Abhradip Saha <110524706+abhradip-saha@users.noreply.github.com>
2024-01-30 14:09:42 +0530

    Update LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -9,7 +9,7 @@ export default function LoginPage() {
   const {setUserInfo} = useContext(UserContext);
   async function login(ev) {
     ev.preventDefault();
-    const response = await fetch('https://blog-backend-auth-alpha.vercel.app/login', {
+    const response = await fetch('https://blog-pki8.onrender.com/login', {
       method: 'POST',
       body: JSON.stringify({username, password}),
       headers: {'Content-Type':'application/json'},
