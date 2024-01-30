2a3c80eb9edbfb11ac0f8a96ba76c75c4a3b5612
Abhradip Saha <110524706+abhradip-saha@users.noreply.github.com>
2024-01-30 13:33:04 +0530

    Update EditPost.js

--- src/pages/EditPost.js
+++ src/pages/EditPost.js
@@ -31,7 +31,7 @@ export default function EditPost() {
     if (files?.[0]) {
       data.set('file', files?.[0]);
     }
-    const response = await fetch('https://blog-64s5.onrender.com/post', {
+    const response = await fetch('https://blog-backend-auth-alpha.vercel.app/post', {
       method: 'PUT',
       body: data,
       credentials: 'include',
@@ -52,7 +52,7 @@ export default function EditPost() {
     if (files?.[0]) {
       data.set('file', files?.[0]);
     }
-    const response = await fetch('https://blog-64s5.onrender.com/post', {
+    const response = await fetch('https://blog-backend-auth-alpha.vercel.app/post', {
       method: 'DELETE',
       body: data,
       credentials: 'include',
